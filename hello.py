f450f013b4231b9ca132314d3765968105905981
bigPerson <daein7076@gmail.com>
2025-04-03 14:40:50 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+# errors and exceptions
+def div21by(divisor):
+    try:
+        return 21 / divisor
+    except:
+        print("Error: Division by zero is not allowed")
+        return None
+    
+
+def div42by(divisor):
+    try:
+        return 42 / divisor
+    except ZeroDivisionError as e:
+        print(f"Error: {e}")
+        return None
+    finally:
+        print("Execution completed")
+
+div42by(0)
