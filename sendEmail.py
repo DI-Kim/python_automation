8cac66cdb6b972d79929b9ab0438f650e673b0cc
bigPerson <daein7076@gmail.com>
2025-04-24 16:40:17 +0900

    send email description

--- sendEmail.py
+++ sendEmail.py
@@ -1,5 +1,6 @@
 import smtplib
 import os
+# pip install python-dotenv
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,6 +16,8 @@ print(conn.starttls())
 
 conn.login(GMAIL_ID, GMAIL_PASSWORD)
 
-conn.sendmail('weaponofjungle@gmail.com', 'daein7076@naver.com', 'Subject: So long...\n\nDear bigPerson, thanks for all the fish\n\n-Daein')
+
+## 보내는 이메일, 받는 이메일, 내용(Subject는 제목, \n\n을 사용하여 본문으로 넘어감)
+conn.sendmail('weaponofjungle@gmail.com', 'daein7076@naver.com', 'Subject: So long...\n\nDear bigPerson, thanks for all the fish\n-Daein')
 
 print(conn.quit())
